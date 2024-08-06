ac4638602a59832f1b6955d2bbe36912d495408b
H Dyson <Hldyson@hotmail.com>
2024-08-06 19:01:02 +0000

    chore: Update background color of page for different screen sizes

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
         className="w-full min-h-screen overflow-auto relative bg-zinc-600 
         sm:bg-zinc-900 sm:w-full  
         md:w-full md:bg-blue-500
-        lg:w-full lg:bg-green-500
+        lg:w-full lg:bg-red-500
         xl:w-full xl:bg-zinc-800"
       >
         <div
